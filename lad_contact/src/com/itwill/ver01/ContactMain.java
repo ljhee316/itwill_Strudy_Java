115d7a1b4cd99b828d1fcf7db31cd535830f4d77
Jeong hee Lee <tpqms316@naver.com>
2024-03-28 19:57:52 +0900

    오류수정해야할것

--- lad_contact/src/com/itwill/ver01/ContactMain.java
+++ lad_contact/src/com/itwill/ver01/ContactMain.java
@@ -6,6 +6,8 @@ public class ContactMain {
 	// 필드
 	private static final int MAX_LENGTH = 3;// 연락처 배열의 최대길이(원소개수)
 
+	// private Contact contact = new Contact();
+
 	private Scanner scanner = new Scanner(System.in);
 	private Contact[] contacts = new Contact[MAX_LENGTH]; // 연락처를 저장하기위한 배열.
 	private int count = 0; // 현재까지 연락처배열에 저장된 개수. 연락처 저장 할 인덱스위치를 정하기위해.
@@ -42,11 +44,12 @@ public class ContactMain {
 		}
 
 		System.out.println("*** 프로그램 종료 ***");
+
 	}
 
 	// 메서드
 	private int showMainMeun() {
-		System.out.println("\n-----------------------------------------------------");
+		System.out.println("\n------------아래 숫자에서 선택하세요----------------");
 		System.out.println("[0]종료 [1]저장 [2]전체목록 [3]인덱스검색 [4]수정");
 		System.out.println("-----------------------------------------------------");
 		System.out.print("선택>>> ");
@@ -83,13 +86,22 @@ public class ContactMain {
 		System.out.println("\n----인덱스 검색----");
 		System.out.print("인덱스 입력>>> ");
 		int index = Integer.parseInt(scanner.nextLine());
-		System.out.println(contacts[index].toString());
+//		if (contacts[index] != null ) {
+//			System.out.println(contacts[index].toString());		
+//		}else if (contacts[index] == null) {
+//			System.out.println("다시 입력해주세요");			
+//		}
+		if (contacts[index] != null) {
+			System.out.println(contacts[index].toString());
+		} else if (contacts[index] == null) {
+			System.out.println("다시 입력해주세요");
+		}
 	}
 
 	private void updateContactByIndex() {
 		System.out.println("\n----연락처 수정----");
 		System.out.print("인덱스 입력>>> ");
-		int index =Integer.parseInt(scanner.nextLine());
+		int index = Integer.parseInt(scanner.nextLine());
 		System.out.println("수정 전: " + contacts[index].toString());
 		System.out.print("이름 수정>>> ");
 		String name = scanner.nextLine();
@@ -100,7 +112,6 @@ public class ContactMain {
 		contacts[index].setName(name);
 		contacts[index].setEmail(email);
 		contacts[index].setPhone(phone);
-		
-		
-}
+
+	}
 }
